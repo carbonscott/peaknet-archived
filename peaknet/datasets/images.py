6d5d7e4b01bcad58dd105d466dc606804f566710
carbonscott <wangimagine@gmail.com>
2022-08-22 16:45:14 -0700

    Remove the sequentail index layer.  Rename some funcitons.  get_ to retrieve infor by index and extract_ retrieve infor by filename.

--- peaknet/datasets/images.py
+++ peaknet/datasets/images.py
@@ -39,6 +39,9 @@ class SFXDataset(Dataset):
     """
     SFX images are collected from multiple datasets specified in the input csv
     file. All images are organized in a plain list.  
+
+    get_     method returns data by sequential index.
+    extract_ method returns object by files.
     """
 
     def __init__(self, config):
@@ -53,11 +56,12 @@ class SFXDataset(Dataset):
         self.trans         = getattr(config, 'trans'         , None)
 
         # Variables that capture raw information from the input (stream files)
-        self.fl_stream_list           = []
-        self.metadata_orig_list       = [] # ...A list of (fl_cxi, event_crystfel) that have labeled peaks
-        self.peak_orig_list           = [] # ...A list of labeled peaks
-        self.img_and_label_cache_dict = {} # ...A list of image data and their corresponding labels
-        self.is_cache                 = False
+        self.fl_stream_list     = []
+        self.metadata_orig_list = [] # ...A list of (fl_cxi, event_crystfel) that have labeled peaks
+        self.peak_orig_list     = [] # ...A list of labeled peaks
+        self.peak_cache_dict    = {} # ...A list of image data and their corresponding labels
+        self.stream_cache_dict  = {} # ...A dictionary of file to stream mapping
+        self.is_cache           = False
 
         # Variables that capture information in data spliting
         self.metadata_list   = []
@@ -79,17 +83,19 @@ class SFXDataset(Dataset):
 
         # Obtain all indexed SFX event from stream files...
         for fl_stream in self.fl_stream_list:
-            # Get the stream object...
-            stream_dict = self.parse_stream(fl_stream)
+            # Get the stream object and save it to a global dictionary...
+            stream_per_file_dict = self.parse_stream(fl_stream)
+            if not fl_stream in self.stream_cache_dict:
+                self.stream_cache_dict[fl_stream] = stream_per_file_dict
 
             # Create a list of data entry from a stream file...
-            metadata_per_stream = self.extract_metadata(stream_dict)
+            metadata_per_stream = self.extract_metadata_from_streamfile(fl_stream)
 
             # Accumulate data...
-            self.metadata_orig_list.extend(metadata_per_stream)
+            self.metadata_orig_list.extend(metadata_per_stream)    # (fl_stream, fl_cxi, event_crystfel)
 
             # Extract all peaks...
-            peak_list_per_stream = self.extract_labeled_peak(stream_dict)
+            peak_list_per_stream = self.extract_labeled_peak_from_streamfile(fl_stream)
             self.peak_orig_list.extend(peak_list_per_stream)
 
         # Split original dataset sequence into training sequence and holdout sequence...
@@ -122,7 +128,7 @@ class SFXDataset(Dataset):
 
     def parse_stream(self, fl_stream):
         # Initialize the object to return...
-        stream_dict = None
+        stream_per_file_dict = None
 
         # Find the basename of the stream file...
         basename_stream = os.path.basename(fl_stream)
@@ -138,72 +144,105 @@ class SFXDataset(Dataset):
         # Obtain key information from stream by loading the pickle file if it exists...
         if os.path.exists(path_pickle):
             with open(path_pickle, 'rb') as fh:
-                stream_dict = pickle.load(fh)
+                stream_per_file_dict = pickle.load(fh)
 
         # Otherwise, parse the stream file...
         else:
             # Employ stream parser to extract key info from stream...
             stream_parser = StreamParser(fl_stream)
             stream_parser.parse()
-            stream_dict = stream_parser.stream_dict
+            stream_per_file_dict = stream_parser.stream_per_file_dict
 
             # Save the stream result in a pickle file...
             with open(path_pickle, 'wb') as fh:
-                pickle.dump(stream_dict, fh, protocol = pickle.HIGHEST_PROTOCOL)
+                pickle.dump(stream_per_file_dict, fh, protocol = pickle.HIGHEST_PROTOCOL)
 
-        return stream_dict
+        return stream_per_file_dict
 
 
-    def extract_metadata(self, stream_dict):
+    def extract_metadata_from_streamfile(self, fl_stream):
+        # Fetch stream either from scratch or from a cached dictionary...
+        stream_per_file_dict = self.parse_stream(fl_stream) if not fl_stream in self.stream_cache_dict \
+                                                            else self.stream_cache_dict[fl_stream]
+
         # Get all filename and crystfel event...
-        fl_cxi = list(stream_dict['chunk'].keys())[0]
-        event_crystfel_list = list(stream_dict['chunk'][fl_cxi].keys())
+        fl_cxi = list(stream_per_file_dict['chunk'].keys())[0]
+        event_crystfel_list = list(stream_per_file_dict['chunk'][fl_cxi].keys())
 
         # Accumulate data for making a label...
-        metadata_list = [ (fl_cxi, event_crystfel) for event_crystfel in event_crystfel_list ]
+        metadata_list = [ (fl_stream, fl_cxi, event_crystfel) for event_crystfel in event_crystfel_list ]
 
         return metadata_list
 
 
-    def extract_labeled_peak(self, stream_dict):
+    def get_raw_peak(self, idx):
+        '''
+        Return raw found peaks and indexed peaks in a tuple.
+        '''
+        # Read image...
+        # Ignore which stream file this information is extracted from
+        fl_stream, fl_cxi, event_crystfel = self.metadata_list[idx]
+
+        # Fetch stream either from scratch or from a cached dictionary...
+        stream_per_file_dict = self.parse_stream(fl_stream) if not fl_stream in self.stream_cache_dict \
+                                                            else self.stream_cache_dict[fl_stream]
+
+        # Get the right chunk
+        chunk_dict = stream_per_file_dict['chunk'][fl_cxi][event_crystfel]
+
+        # Find all peaks...
+        found_per_event_dict   = {}
+        indexed_per_event_dict = {}
+        for panel, peak_saved_dict in chunk_dict.items():
+            found_per_event_dict[panel]   = peak_saved_dict['found']
+            indexed_per_event_dict[panel] = peak_saved_dict['indexed']
+
+        return found_per_event_dict, indexed_per_event_dict
+
+
+    def extract_labeled_peak_from_streamfile(self, fl_stream):
+        # Fetch stream either from scratch or from a cached dictionary...
+        stream_per_file_dict = self.parse_stream(fl_stream) if not fl_stream in self.stream_cache_dict \
+                                                            else self.stream_cache_dict[fl_stream]
+
         # Get all filename and crystfel event...
-        fl_cxi = list(stream_dict['chunk'].keys())[0]
-        event_crystfel_list = list(stream_dict['chunk'][fl_cxi].keys())
+        fl_cxi = list(stream_per_file_dict['chunk'].keys())[0]
+        event_crystfel_list = list(stream_per_file_dict['chunk'][fl_cxi].keys())
 
         # Only keep those both found and indexed peaks...
         peak_list = []
         for event_crystfel in event_crystfel_list:
             # Get the right chunk
-            chunk_dict = stream_dict['chunk'][fl_cxi][event_crystfel]
+            chunk_dict = stream_per_file_dict['chunk'][fl_cxi][event_crystfel]
 
             # Find all peaks...
-            peak_dict    = {}
-            indexed_dict = {}
+            found_per_event_dict   = {}
+            indexed_per_event_dict = {}
             for panel, peak_saved_dict in chunk_dict.items():
-                peak_dict[panel]    = peak_saved_dict['found']
-                indexed_dict[panel] = peak_saved_dict['indexed']
+                found_per_event_dict[panel]   = peak_saved_dict['found']
+                indexed_per_event_dict[panel] = peak_saved_dict['indexed']
 
             # Filter peaks and only keep those that are indexed...
             peak_list_per_chunk = []
-            for panel, peak_found_list in peak_dict.items():
+            for panel, found_list in found_per_event_dict.items():
                 # Skp panels that have no peaks found...
-                if not panel in indexed_dict: continue
+                if not panel in indexed_per_event_dict: continue
 
                 # Skp panel that have peaks found but nothing indexed...
-                indexed_list = indexed_dict[panel]
+                indexed_list = indexed_per_event_dict[panel]
                 if len(indexed_list) == 0: continue
 
-                peak_list_tree    = cKDTree(peak_found_list)
+                found_list_tree   = cKDTree(found_list)
                 indexed_list_tree = cKDTree(indexed_list)
 
                 # Filter based on a thresholding distance that tolerates the separation between a found peak and an indexed peak...
-                idx_indexed_peak_list = peak_list_tree.query_ball_tree(indexed_list_tree, r = self.dist)
+                idx_indexed_peak_list = found_list_tree.query_ball_tree(indexed_list_tree, r = self.dist)
 
                 # Extract filtered peak positions...
                 for idx, neighbor_list in enumerate(idx_indexed_peak_list):
                     if len(neighbor_list) == 0: continue
 
-                    x, y = peak_found_list[idx]
+                    x, y = found_list[idx]
 
                     peak_list_per_chunk.append((int(x), int(y)))
 
@@ -211,7 +250,6 @@ class SFXDataset(Dataset):
             peak_list.append(peak_list_per_chunk)
 
         return peak_list
-
 
 
     def __len__(self):
@@ -222,22 +260,23 @@ class SFXDataset(Dataset):
         ''' Cache image in the seq_random_list unless a subset is specified.
         '''
         # If subset is not give, then go through the whole set...
-        if not len(idx_list): idx_list = range(list(self.metadata_list))
+        if not len(idx_list): idx_list = range(len(self.metadata_list))
 
         for idx in idx_list:
             # Skip those have been recorded...
-            if idx in self.img_and_label_cache_dict: continue
+            if idx in self.peak_cache_dict: continue
 
             # Otherwise, record it
             img, label = self.get_img_and_label(idx, verbose = True)
-            self.img_and_label_cache_dict[idx] = (img, label)
+            self.peak_cache_dict[idx] = (img, label)
 
         return None
 
 
     def get_img_and_label(self, idx, verbose = False):
         # Read image...
-        fl_cxi, event_crystfel = self.metadata_list[idx]
+        # Ignore which stream file this information is extracted from
+        _, fl_cxi, event_crystfel = self.metadata_list[idx]
         with h5py.File(fl_cxi, 'r') as fh:
             img = fh["/entry_1/instrument_1/detector_1/data"][event_crystfel]
         size_y, size_x = img.shape
@@ -266,7 +305,7 @@ class SFXDataset(Dataset):
 
 
     def __getitem__(self, idx):
-        img, label = self.img_and_label_cache_dict[idx] if   idx in self.img_and_label_cache_dict \
+        img, label = self.peak_cache_dict[idx] if   idx in self.peak_cache_dict \
                                                         else self.get_img_and_label(idx)
 
         # Apply any possible transformation...
@@ -286,11 +325,11 @@ class SFXDataset(Dataset):
 
 
 
-class MiniSFXDataset(SFXDataset):
+class SFXDatasetMini(SFXDataset):
     def __init__(self, config):
         super().__init__(config)
 
-        self.idx_metadata_subset_list = self.form_subset()
+        self.form_miniset()
 
 
     def __len__(self):
@@ -298,20 +337,18 @@ class MiniSFXDataset(SFXDataset):
 
 
     def __getitem__(self, idx):
-        # Find its real index in metadata_list...
-        idx_metadata = self.idx_metadata_subset_list[idx]
-
-        # Access the indexed data in metadata_list...
-        img, label = super().__getitem__(idx_metadata)
+        img, label = super().__getitem__(idx)
 
         return img, label
 
 
-    def form_subset(self):
+    def form_miniset(self):
         size_sample = self.size_sample
 
+        # Draw a miniset (subset)...
         idx_metadata_list = range(len(self.metadata_list))
+        idx_metadata_miniset = random.sample(idx_metadata_list, k = size_sample)
 
-        idx_metadata_subset_list = random.sample(idx_metadata_list, k = size_sample)
-
-        return idx_metadata_subset_list
+        # Reconstruct metadata_list and peak_list...
+        self.metadata_list = [ self.metadata_list[i] for i in idx_metadata_miniset ]
+        self.peak_list     = [ self.peak_list[i]     for i in idx_metadata_miniset ]

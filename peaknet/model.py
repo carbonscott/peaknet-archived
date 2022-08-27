58e4a5ed5650c3482ff06b084b121e779a2b5630
carbonscott <wangimagine@gmail.com>
2022-08-26 22:44:56 -0700

    Use positive weighting in BCE loss to tackle the unbalanced dataset issue (most pixels are black or zero).

--- peaknet/model.py
+++ peaknet/model.py
@@ -25,30 +25,80 @@ class PeakFinderModel(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        self.method = config.method
+        self.method     = config.method
+        self.pos_weight = config.pos_weight * torch.ones([1])
 
-        self.loss_func = nn.BCELoss()
-        self.sigmoid   = nn.Sigmoid()
+        self.BCEWithLogitsLoss = nn.BCEWithLogitsLoss(pos_weight = self.pos_weight)
+        self.Sigmoid = nn.Sigmoid()
+        self.MSELoss = nn.MSELoss()
 
 
     def forward(self, batch_img, batch_mask):
         # Find the predicted mask...
         batch_mask_predicted = self.method.forward(batch_img)
 
-        # Pass through sigmoid...
-        batch_mask_predicted_with_sigmoid = self.sigmoid(batch_mask_predicted)
-
         # Crop the target mask...
-        size_y, size_x = batch_mask_predicted_with_sigmoid.shape[-2:]
-        batch_mask_crop = center_crop(batch_mask, size_y, size_x)
+        size_y, size_x = batch_mask_predicted.shape[-2:]
+        batch_mask_true = center_crop(batch_mask, size_y, size_x)
 
-        # Calculate loss...
-        loss = self.loss_func(batch_mask_predicted_with_sigmoid, batch_mask_crop)
+        # Calculate BCE loss...
+        loss_bce = self.BCEWithLogitsLoss(batch_mask_predicted, batch_mask_true)
 
-        return batch_mask_predicted_with_sigmoid, batch_mask_crop, loss
+        loss = loss_bce
+
+        return batch_mask_predicted, batch_mask_true, loss
 
 
     def configure_optimizers(self, config_train):
         optimizer = torch.optim.Adam(self.method.parameters(), lr = config_train.lr)
 
         return optimizer
+
+
+    def calc_dice_loss(self, batch_mask_predicted, batch_mask_true, smooth = 1.0):
+        # Calculate batch intersection...
+        batch_mask_intersection = batch_mask_predicted * batch_mask_true
+        batch_mask_intersection_val = batch_mask_intersection.sum(dim = (-2, -1))
+
+        # Calculate the dice coefficient...
+        batch_dice_coeff  = 2.0 * batch_mask_intersection_val
+        batch_dice_coeff /= batch_mask_predicted.sum(dim = (-2, -1)) \
+                          + batch_mask_true.sum(dim = (-2, -1))      \
+                          + smooth
+
+        return -batch_dice_coeff.mean()
+
+
+    def calc_iou_loss(self, batch_mask_predicted, batch_mask_true, smooth = 1.0):
+        # Calculate batch intersection...
+        batch_mask_intersection = batch_mask_predicted * batch_mask_true
+        batch_mask_intersection_sum = batch_mask_intersection.sum(dim = (-2, -1))
+
+        # Calculate the iou score...
+        batch_iou  = batch_mask_intersection_sum + 1.0
+        batch_iou /= batch_mask_predicted.sum(dim = (-2, -1)) \
+                   + batch_mask_true.sum(dim = (-2, -1))      \
+                   - batch_mask_intersection_sum              \
+                   + 1.0
+
+        return -batch_iou.mean()
+
+
+
+
+    def calc_focal_loss(self, inputs, targets, alpha = 0.8, gamma = 2.0, smooth=1):
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = nn.Sigmoid()(inputs)
+
+        #flatten label and prediction tensors
+        ## inputs = inputs.view(-1)
+        ## targets = targets.view(-1)
+        inputs  = torch.flatten(inputs , start_dim = -2)
+        targets = torch.flatten(targets, start_dim = -2)
+
+        #first compute binary cross-entropy 
+        BCE = nn.functional.binary_cross_entropy(inputs, targets, reduction='mean')
+        BCE_EXP = torch.exp(-BCE)
+        focal_loss = alpha * (1-BCE_EXP)**gamma * BCE
+
+        return focal_loss.mean()

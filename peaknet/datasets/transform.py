fcd7d7ff52d057ded0a543f835f05005ea52c647
carbonscott <wangimagine@gmail.com>
2022-08-31 12:02:54 -0700

    Identified the root cause of the offset which lies in the cropping step.  There is a risk that every cropping has a 0.5 pixel shift towards the origin caused by integer division in programming language like Python.  Solution is to keep track of offset.

--- peaknet/datasets/transform.py
+++ peaknet/datasets/transform.py
@@ -3,7 +3,11 @@
 
 import torch
 
-def center_crop(img, size_y_crop, size_x_crop):
+def center_crop(img, size_y_crop, size_x_crop, return_offset_ok = False):
+    '''
+    Return the cropped area and associated offset for coordinate transformation
+    purposes.  
+    '''
     # Get metadata (device)...
     device = img.device
 
@@ -19,6 +23,7 @@ def center_crop(img, size_y_crop, size_x_crop):
     img_super = torch.zeros((*dim_storage, size_y_super, size_x_super), device = device)
 
     # Paint the original image onto the super image...
+    # Area
     y_min_img = (size_y_super - size_y_img) // 2
     x_min_img = (size_x_super - size_x_img) // 2
     img_super[...,
@@ -39,12 +44,24 @@ def center_crop(img, size_y_crop, size_x_crop):
                           y_min_crop : y_max_crop,
                           x_min_crop : x_max_crop]
 
-    return img_crop
+    # Pack things to return in a tuple
+    ret_tuple = img_crop
+    if return_offset_ok:
+        # Min float for finding the offset
+        y_min_crop_float = (size_y_super - size_y_crop) / 2
+        x_min_crop_float = (size_x_super - size_x_crop) / 2
+
+        # Offset introduced due to the integer division
+        offset_tuple = (y_min_crop_float - y_min_crop, x_min_crop_float - x_min_crop)
+
+        ret_tuple = (img_crop, offset_tuple)
+
+    return ret_tuple
 
 
 
 
-def coord_img_to_crop(coord_tuple, size_img_tuple, size_crop_tuple):
+def coord_img_to_crop(coord_tuple, size_img_tuple, size_crop_tuple, offset_tuple = ()):
     '''
     Need some unit test.
     '''
@@ -56,5 +73,10 @@ def coord_img_to_crop(coord_tuple, size_img_tuple, size_crop_tuple):
     # Transform...
     y_crop = (size_y_crop - size_y_img) / 2 + y
     x_crop = (size_x_crop - size_x_img) / 2 + x
+
+    if len(offset_tuple) == 2:
+        y_offset, x_offset = offset_tuple
+        y_crop += y_offset
+        x_crop += x_offset
 
     return y_crop, x_crop

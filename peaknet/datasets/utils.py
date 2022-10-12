8cfba86c289cbbc916e33474eddcd8ab19c555a6
carbonscott <wangimagine@gmail.com>
2022-10-12 10:33:02 -0700

    Provide new features: mask and assemble.

--- peaknet/datasets/utils.py
+++ peaknet/datasets/utils.py
@@ -21,6 +21,12 @@ class PsanaImg:
         # Set up detector
         self.detector = psana.Detector(detector_name)
 
+        # Set image reading mode
+        self.read = { "raw"   : self.detector.raw,
+                      "calib" : self.detector.calib,
+                      "image" : self.detector.image,
+                      "mask"  : self.detector.mask, }
+
 
     def __len__(self):
         return len(self.timestamps)
@@ -38,10 +44,33 @@ class PsanaImg:
             f"Mode {mode} is not allowed!!!  Only 'raw', 'calib' and 'image' are supported."
 
         # Fetch image data based on timestamp from detector...
-        read = { "raw"   : self.detector.raw,
-                 "calib" : self.detector.calib,
-                 "image" : self.detector.image,}
-        data = read[mode](event)
+        data = self.read[mode](event)
         img  = data[int(id_panel)] if id_panel is not None else data
 
         return img
+
+
+    def assemble(self, multipanel = None, mode = "image", fake_event_num = 0):
+        # Set up a fake event_num...
+        event_num = fake_event_num
+
+        # Fetch the timestamp according to event number...
+        timestamp = self.timestamps[int(event_num)]
+
+        # Access each event based on timestamp...
+        event = self.run_current.event(timestamp)
+
+        # Fetch image data based on timestamp from detector...
+        img = self.read[mode](event, multipanel)
+
+        return img
+
+
+    def create_bad_pixel_mask(self):
+        return self.read["mask"](self.run_current, calib       = True,
+                                                   status      = True,
+                                                   edges       = True,
+                                                   central     = True,
+                                                   unbond      = True,
+                                                   unbondnbrs  = True,
+                                                   unbondnbrs8 = False).astype(np.uint16)

c65acfade5e49ad80f1c6c17c1519b242ad1f8f1
carbonscott <wangimagine@gmail.com>
2022-11-07 13:15:45 -0800

    Provide function of runtime performance measure.

--- peaknet/predictor.py
+++ peaknet/predictor.py
@@ -89,3 +89,80 @@ class CheetahPeakFinder:
                 peak_list.append((y, x))
 
         return peak_list
+
+
+    def find_peak_and_perf(self, img_stack, threshold_prob = 1 - 1e-4, min_num_peaks = 15):
+        import time
+
+        peak_list = []
+
+        time_start = time.time()
+        # Normalize the image stack...
+        img_stack = (img_stack - img_stack.mean(axis = (2, 3), keepdim = True)) / img_stack.std(axis = (2, 3), keepdim = True)
+        time_end = time.time()
+        time_delta = time_end - time_start
+        time_delta_name = 'pf:Normalization'
+        print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        time_start = time.time()
+        # Get activation feature map given the image stack...
+        self.model.eval()
+        with torch.no_grad():
+            fmap_stack = self.model.forward(img_stack)
+        time_end = time.time()
+        time_delta = time_end - time_start
+        time_delta_name = 'pf:Inference'
+        print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        time_start = time.time()
+        # Convert to probability with the sigmoid function...
+        mask_stack_predicted = fmap_stack.sigmoid()
+        time_end = time.time()
+        time_delta = time_end - time_start
+        time_delta_name = 'pf:Sigmoid'
+        print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        time_start = time.time()
+        # Thresholding the probability...
+        mask_stack_predicted[  mask_stack_predicted < threshold_prob ] = 0
+        mask_stack_predicted[~(mask_stack_predicted < threshold_prob)] = 1
+        time_end = time.time()
+        time_delta = time_end - time_start
+        time_delta_name = 'pf:Thresholding'
+        print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        time_start = time.time()
+        # Find center of mass for each image in the stack...
+        num_stack, _, size_y, size_x = mask_stack_predicted.shape
+        peak_pos_predicted_stack = self.calc_batch_center_of_mass(mask_stack_predicted.view(num_stack, size_y, size_x))
+        time_end = time.time()
+        time_delta = time_end - time_start
+        time_delta_name = 'pf:Center of mass'
+        print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        # A workaround to avoid copying gpu memory to cpu when num of peaks is small...
+        if len(peak_pos_predicted_stack) >= min_num_peaks:
+            time_start = time.time()
+            # Convert to cheetah coordinates...
+            for peak_pos in peak_pos_predicted_stack:
+                idx_panel, y, x = peak_pos.get()
+
+                if isnan(y) or isnan(x): continue
+
+                idx_panel = int(idx_panel)
+
+                y, x = coord_crop_to_img((y, x), img_stack.shape[-2:], mask_stack_predicted.shape[-2:])
+
+                x_min, y_min, x_max, y_max = self.cheetah_geom_list[idx_panel]
+
+                x += x_min
+                y += y_min
+
+                peak_list.append((y, x))
+
+            time_end = time.time()
+            time_delta = time_end - time_start
+            time_delta_name = 'pf:Save coordinates'
+            print(f"Time delta ({time_delta_name:20s}): {time_delta * 1e3:.4f} ms.")
+
+        return peak_list

6a4c59f9bb0da2e7b632f90ebb6b144756fe39f2
carbonscott <wangimagine@gmail.com>
2023-04-26 20:03:07 -0700

    Uses explicit logic.

--- peaknet/utils.py
+++ peaknet/utils.py
@@ -26,11 +26,12 @@ def set_seed(seed):
 
 class EpochManager:
 
-    def __init__(self, trainer, validator, device = 0):
+    def __init__(self, trainer, validator, saves_checkpoint = True):
 
         self.trainer   = trainer
         self.validator = validator
-        self.device  = device    # 0 if only one gpu is used.
+
+        self.saves_checkpoint = saves_checkpoint
 
         # Track model training information
         self.param_update_ratio_dict  = {}
@@ -87,7 +88,7 @@ class EpochManager:
         # Save checkpoint whenever validation loss gets smaller...
         # Notice it doesn't imply early stopping
         if loss_validate < self.loss_min:
-            if self.device == 0:
+            if self.save_checkpoint:
                 # Save a checkpoint file...
                 self.trainer.save_checkpoint(epoch = epoch)
 
